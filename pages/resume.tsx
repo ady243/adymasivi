fdf69383951edbe2d988fff0e01e3eb1b5c2511c
ady masivi <hardest12>
2022-05-23 14:36:16 +0200

    resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -31,6 +31,51 @@ function resume() {
           </div>
         </div>
       </div>
+      <div className="grid md:grid-cols-2 gap-6">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Educations</h5>
+          <div>
+            <h5 className="my-2 text-xl  font-bold">CFA SUP DE VINCI</h5>
+            <p className="font-semibold">
+              Ecole d'igéniérie informatique (2021-2022)
+            </p>
+            <p className="my-3">
+              Poursuit actuellement un cursus d'ingeniérie informatique
+            </p>
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Expérience</h5>
+          <div>
+            <h5 className="my-2 text-xl  font-bold">CFA SUP DE VINCI</h5>
+            <p className="font-semibold">
+              Ecole d'igéniérie informatique (2021-2022)
+            </p>
+            <p className="my-3">
+              Poursuit actuellement un cursus d'ingeniérie informatique
+            </p>
+          </div>
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2">
+          <div className="my-3 text-2xl fonr-bold">
+            <div className="my-2">
+              {languages.map((languages) => (
+                <Bar data={languages} key={languages.name} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="grid gap-6 md:grid-cols-2">
+          <div className="my-3 text-2xl fonr-bold">
+            <div className="my-2">
+              {tools.map((tool) => (
+                <Bar data={tool} key={tool.name} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
